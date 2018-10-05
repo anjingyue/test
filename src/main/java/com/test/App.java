578466123c230517f41ed3553dc63df9573cc3c4
chenyu <409316036@qq.com>
2018-10-05 21:25:16 +0800

    add ##

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -4,6 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("******************#");
         System.out.println("HelloWorld!!!");
-        System.out.println("******************");
+        System.out.println("******************#");
     }
 }
