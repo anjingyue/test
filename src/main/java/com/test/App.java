c9c68f46d021c5415956b3a21642eb4f10176fcf
chenjingyue <1544552008@qq.com>
2018-10-06 11:28:47 +0800

    remove ##

--- src/main/java/com/test/App.java
+++ src/main/java/com/test/App.java
@@ -2,8 +2,8 @@ package com.test;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("******************#");
+        System.out.println("******************");
         System.out.println("HelloWorld!!!");
-        System.out.println("******************#");
+        System.out.println("******************");
     }
 }
